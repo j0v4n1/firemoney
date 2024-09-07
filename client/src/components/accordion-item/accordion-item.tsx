8d7881d69d3b52ce3dd8830f2306559814b452cf
Ivan Klimovskiy <klimoffsky27@gmail.com>
2024-09-07 13:07:14 +0300

    доделать аккордион

--- client/src/components/accordion-item/accordion-item.tsx
+++ client/src/components/accordion-item/accordion-item.tsx
@@ -5,23 +5,43 @@ import { AccordionItemProps } from './accordion-item.types';
 export default function AccordionItem({ id, header, body }: AccordionItemProps) {
   const [isHidden, setIsHidden] = useState(true);
   const [itemId, setItemId] = useState<number | null>(null);
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
   return (
-    <div key={id} className={styles['accordion__item']}>
+    <div
+      key={id}
+      className={
+        (!isHidden && itemId === id) || isFirstRender
+          ? `${styles['accordion__item']} ${styles['accordion__item_background']}`
+          : styles['accordion__item']
+      }>
       <div
         onClick={() => {
+          setIsFirstRender(false);
           setItemId(id);
           if (id === itemId) {
             setIsHidden(!isHidden);
           }
         }}
         className={styles['accordion__header-wrapper']}>
-        <h3 className={styles['accordion__header']}>{header}</h3>
-        <button className={styles['accordion__btn']}></button>
+        <h3
+          className={
+            (!isHidden && itemId === id) || isFirstRender
+              ? `${styles['accordion__header']}`
+              : `${styles['accordion__header']}  ${styles['accordion__header_padding']}`
+          }>
+          {header}
+        </h3>
+        <div
+          className={
+            (!isHidden && itemId === id) || isFirstRender
+              ? `${styles['accordion__arrow']} ${styles['accordion__arrow_rotate']}`
+              : styles['accordion__arrow']
+          }></div>
       </div>
       <p
         className={
-          isHidden && itemId === id
+          (!isHidden && itemId === id) || isFirstRender
             ? `${styles['accordion__body']} ${styles['accordion__body_hidden']}`
             : styles['accordion__body']
         }>
